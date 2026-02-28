f7dae2f713a7054a199fc14f718f3548a205afdb
Jimmy Piedrahita <jimmy22piedrahita@gmail.com>
2026-02-27 21:51:56 -0500

    Add Nodo class for doubly linked list implementation

---
+++ src/Nodo.java
@@ -0,0 +1,11 @@
+public class Nodo<T> {
+    T valor;
+    Nodo<T> siguiente;
+    Nodo<T> anterior;
+
+    public Nodo(T valor) {
+        this.valor = valor;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
